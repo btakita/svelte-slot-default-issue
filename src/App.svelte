cbd01a9905db0cf578bbf17373f1eb8769df969c
Brian Takita <brian.takita@gmail.com>
2019-04-10 15:27:33 -0400

    Run `yarn run start` to demonstrate the issue

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Component from './Component.svelte'
+</script>
+
+<Component href='.'>
+	<div slot=test2>
+		override
+	</div>
+	Rest of slot
+</Component>
---
+++ src/Component.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let href
+</script>
+
+<div class=Component>
+	<slot name=test>
+		<div class=test__Component>
+			<slot name=test1>
+				<a {href}>
+					<slot name=test2>
+						default value
+					</slot>
+				</a>
+			</slot>
+		</div>
+	</slot>
+	<slot></slot>
+</div>
